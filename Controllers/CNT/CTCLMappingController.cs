553f60071a8188da72b2c0dfed9c16ad7c2d1e3c
lkp-pranav <pranav.bhide@lkpsec.com>
2025-04-22 16:02:13 +0530

    add ctcl mapping functionality

---
+++ Controllers/CNT/CTCLMappingController.cs
@@ -0,0 +1,51 @@
+﻿using LKP_Frontend_MVC.Models.Request.Common;
+using LKP_Frontend_MVC.Models.Response.ClientDealer;
+using LKP_Frontend_MVC.Models.Response.Common;
+using LKP_Frontend_MVC.Models.Response.CTCLMapping;
+using LKP_Frontend_MVC.Models.Response.User;
+using LKP_Frontend_MVC.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace LKP_Frontend_MVC.Controllers.CNT
+{
+    public class CTCLMappingController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public CTCLMappingController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index(PageInputModel inputModel)
+        {
+            string sessionUserJson = HttpContext.Session.GetString("sessionUser");
+            if (sessionUserJson == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            var sessionUser = JsonConvert.DeserializeObject<SessionUser>(sessionUserJson);
+            inputModel.user_id = sessionUser.user_id;
+            inputModel.user_type = sessionUser.user_type;
+
+            ResponsePayLoad responsePayLoad = new ResponsePayLoad();
+            List<CTCLMappingResponse> model = new List<CTCLMappingResponse>();
+
+            string url = "https://localhost:7121/api/CTCLMapping/GetAllMapping";
+            responsePayLoad = await LoginHelper.SendHttpRequest(_httpClient, url, inputModel, "Bearer", sessionUser.accessToken);
+
+            if (responsePayLoad == null || !responsePayLoad.isSuccess)
+            {
+                return View(new List<CTCLMappingResponse>());
+            }
+
+            model = JsonConvert.DeserializeObject<List<CTCLMappingResponse>>(responsePayLoad.data.ToString());
+            responsePayLoad.data = model;
+            responsePayLoad.message = inputModel;
+
+            return View(responsePayLoad);
+        }
+    }
+}
---
+++ Models/Response/CTCLMapping/CTCLMappingResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace LKP_Frontend_MVC.Models.Response.CTCLMapping
+{
+    public class CTCLMappingResponse
+    {
+        public string? Clientcode { get; set; } = "";
+        public string? PrimaryDealer { get; set; }
+        public string? PrimaryDealerName { get; set; }
+        public string? PrimaryDealerCTCLLogin { get; set; }
+        public string? PrimaryDealerWorkStatus { get; set; }
+        public string? SecondaryDealer { get; set; }
+        public string? SecondaryDealerName { get; set; }
+        public string? SecondaryDealerCTCLLogin { get; set; }
+        public string? SecondaryDealerWorkStatus { get; set; }
+        public string? Zone { get; set; }
+        public string? ZoneDealerID { get; set; }
+        public string? ZoneDealerName { get; set; }
+        public string? ZoneCtclLoginID { get; set; }
+        public string? ZoneWorkStatus { get; set; }
+        public int IsHOCNT { get; set; }
+        public string? HO_DealerID { get; set; }
+        public string? HO_DealerName { get; set; }
+        public string? HO_CTCLLogin { get; set; }
+        public string? HO_WorkStatus { get; set; }
+        public string? Category { get; set; }
+        public int Flag { get; set; }
+    }
+}
