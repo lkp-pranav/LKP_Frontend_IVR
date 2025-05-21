dbf74d30c4ec68f56fa3610e637f53d22958424f
lkp-pranav <pranav.bhide@lkpsec.com>
2025-05-21 10:40:10 +0530

    refactor models

--- Models/Request/BranchCNT/BranchCNTInputModel.cs
+++ Models/Request/BranchCNT/BranchCNTInputModel.cs
@@ -1,13 +1,23 @@
 ﻿using LKP_Frontend_MVC.Models.Request.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace LKP_Frontend_MVC.Models.Request.BranchCNT
 {
     public class BranchCNTInputModel : CommonModel
     {
-        public int RowId { get; set; }  
+        public int RowId { get; set; }
+
+        [Required]
         public string Zone { get; set; }
+
+        [Required]
         public string DealerID { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Dealer Name can't exceed 100 characters.")]
         public string DealerName { get; set; }
+
+        [Required]
         public string CtclLoginId { get; set; }
     }
 }
